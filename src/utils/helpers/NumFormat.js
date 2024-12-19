e522b9ac5d25fe6592343bc30a51671b02339f76
Jmb-924 <jmb11082004@gmail.com>
2024-12-19 18:00:53 -0500

    Limpieza De Arhivos

--- src/utils/helpers/NumFormat.js
+++
@@ -1,3 +0,0 @@
-export function formatNumPrecio(num) {
-   return num.toLocaleString('es-ES'); // Utiliza 'es-ES' para el formato con punto como separador de miles
-}
--- src/utils/helpers/ReplaceBlankSpace.js
+++
@@ -1,6 +0,0 @@
-export function ReplaceSpaceTo(dato, replace) {
-   if (!dato.includes(' ')) {
-      return dato
-   }
-   return dato.replace(/ /g, replace)
-}
